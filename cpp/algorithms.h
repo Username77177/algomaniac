c27a3baf9ca9336872793c9a4f9f4f315ef5d989
username77177 <username77177@gmail.com>
2020-05-13 23:30:04 +0300

    Num System update and template for functions in algorithms

--- cpp/algorithms.h
+++ cpp/algorithms.h
@@ -1,114 +1,33 @@
 #include <iostream>
 
 using namespace std;
-// Bubble sort
-void bubbleSort(
-    int *array,
-    int arraySize); // Функция, которая сортирует массив пузырьковой сортировкой
-void bubbleSortInverse(int *array,
-                       int arraySize); // Функция, которая сортирует массив
-                                       // пузырьковой сортировкой (инверсия)
-void bubbleSort(
-    float *array,
-    int arraySize); // Функция, которая сортирует массив пузырьковой сортировкой
-void bubbleSortInverse(float *array,
-                       int arraySize); // Функция, которая сортирует массив
-                                       // пузырьковой сортировкой (инверсия)
-void bubbleSort(
-    double *array,
-    int arraySize); // Функция, которая сортирует массив пузырьковой сортировкой
-void bubbleSortInverse(double *array,
-                       int arraySize); // Функция, которая сортирует массив
-                                       // пузырьковой сортировкой (инверсия)
-
-// Selection sort
-void selectionSort(
-    int *array, int arraySize); // Функция, которая сортирует массив выборкой
-void selectionSortInverse(
-    int *array, int arraySize); // Функция, которая сортирует массив выборкой
-void selectionSort(
-    float *array, int arraySize); // Функция, которая сортирует массив выборкой
-void selectionSortInverse(
-    float *array, int arraySize); // Функция, которая сортирует массив выборкой
-void selectionSort(
-    double *array,
-    int arraySize); // Функция, которая сортирует массив выборкой
-void selectionSortInverse(
-    double *array,
-    int arraySize); // Функция, которая сортирует массив выборкой
-
-// Insertion sort
-void insertionSort(int *array, int arraySize); // Сортировка массива вставками
-void insertionSortInverse(int *array, int arraySize); // Сортировка массива вставками
-void insertionSort(float *array, int arraySize); // Сортировка массива вставками
-void insertionSortInverse(float *array, int arraySize); // Сортировка массива вставками
-void insertionSort(double *array, int arraySize); // Сортировка массива вставками
-void insertionSortInverse(double *array, int arraySize); // Сортировка массива вставками
-
-void fastSort(int *array, int arraySize); // Быстрая сортировка
-
-void bubbleSort(int *array, int arraySize) {
-  for (int i = 0; i < arraySize - 1; i++) {
-    for (int j = 0; j < arraySize - i - 1; j++) {
-      if (array[j] > array[j + 1]) {
-        swap(array[j], array[j + 1]);
-      }
-    }
-  }
-}
-
-void bubbleSortInverse(int *array, int arraySize) {
-  for (int i = 0; i < arraySize - 1; i++) {
-    for (int j = 0; j < arraySize - i - 1; j++) {
-      if (array[j] < array[j + 1]) {
-        swap(array[j], array[j + 1]);
-      }
-    }
-  }
-}
-
-void bubbleSort(float *array, int arraySize) {
-  for (int i = 0; i < arraySize - 1; i++) {
-    for (int j = 0; j < arraySize - i - 1; j++) {
-      if (array[j] > array[j + 1]) {
-        swap(array[j], array[j + 1]);
-      }
-    }
-  }
-}
-
-void bubbleSortInverse(float *array, int arraySize) {
-  for (int i = 0; i < arraySize - 1; i++) {
-    for (int j = 0; j < arraySize - i - 1; j++) {
-      if (array[j] < array[j + 1]) {
-        swap(array[j], array[j + 1]);
-      }
-    }
-  }
-}
-
-void bubbleSort(double *array, int arraySize) {
-  for (int i = 0; i < arraySize - 1; i++) {
-    for (int j = 0; j < arraySize - i - 1; j++) {
-      if (array[j] > array[j + 1]) {
-        swap(array[j], array[j + 1]);
-      }
-    }
-  }
-}
-
-void bubbleSortInverse(double *array, int arraySize) {
-  for (int i = 0; i < arraySize - 1; i++) {
-    for (int j = 0; j < arraySize - i - 1; j++) {
-      if (array[j] < array[j + 1]) {
-        swap(array[j], array[j + 1]);
-      }
-    }
-  }
-}
 
 // Bubble sort
-void selectionSort(int *array, int arraySize) {
+template <typename tempVar>
+void bubbleSort(tempVar *array, int arraySize) {
+  for (int i = 0; i < arraySize - 1; i++) {
+    for (int j = 0; j < arraySize - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        swap(array[j], array[j + 1]);
+      }
+    }
+  }
+}
+
+template <typename tempVar>
+void bubbleSortInverse(tempVar *array, int arraySize) {
+  for (int i = 0; i < arraySize - 1; i++) {
+    for (int j = 0; j < arraySize - i - 1; j++) {
+      if (array[j] < array[j + 1]) {
+        swap(array[j], array[j + 1]);
+      }
+    }
+  }
+}
+
+// Selection Sort
+template<typename var>
+void selectionSort(var *array, int arraySize) {
   for (int i = 0; i < arraySize - 1; i++) {
     for (int j = i + 1; j < arraySize - 1; j++) {
       if (array[i] > array[j]) {
@@ -118,7 +37,8 @@ void selectionSort(int *array, int arraySize) {
   }
 }
 
-void selectionSortInverse(int *array, int arraySize) {
+template<typename var>
+void selectionSortInverse(var *array, int arraySize) {
   for (int i = 0; i < arraySize - 1; i++) {
     for (int j = i + 1; j < arraySize; j++) {
       if (array[i] < array[j]) {
@@ -128,45 +48,6 @@ void selectionSortInverse(int *array, int arraySize) {
   }
 }
 
-void selectionSort(float *array, int arraySize) {
-  for (int i = 0; i < arraySize - 1; i++) {
-    for (int j = i + 1; j < arraySize - 1; j++) {
-      if (array[i] > array[j]) {
-        swap(array[i], array[j]);
-      }
-    }
-  }
-}
-
-void selectionSortInverse(float *array, int arraySize) {
-  for (int i = 0; i < arraySize - 1; i++) {
-    for (int j = i + 1; j < arraySize; j++) {
-      if (array[i] < array[j]) {
-        swap(array[i], array[j]);
-      }
-    }
-  }
-}
-
-void selectionSort(double *array, int arraySize) {
-  for (int i = 0; i < arraySize - 1; i++) {
-    for (int j = i + 1; j < arraySize - 1; j++) {
-      if (array[i] > array[j]) {
-        swap(array[i], array[j]);
-      }
-    }
-  }
-}
-
-void selectionSortInverse(double *array, int arraySize) {
-  for (int i = 0; i < arraySize - 1; i++) {
-    for (int j = i + 1; j < arraySize; j++) {
-      if (array[i] < array[j]) {
-        swap(array[i], array[j]);
-      }
-    }
-  }
-}
 
 //TODO
 // Insertion Sort
@@ -207,7 +88,8 @@ for (int i = 1; i < arraySize; i++)
 // 3. Инициализируем переменную, которая будет указывать на все элементы, что левее pivot
 // 4. Меняем элементы. Теперь элемент, который меньше pivot будет первым. Инкрементируем переменную, дабы при следующем нахождении, элемент который будет найден будет стоять на 2 месте
 // 5. Если цикл дошёл до конца, то инкрементируем переменную, и ставим pivot на данный индекс
-int partition (int arr[], int low, int high)  
+template<typename var>
+int partition (var arr[], int low, int high)  
 {  
     int pivot = arr[high];
     for (int j = low; j < high; j++)  
@@ -221,7 +103,8 @@ int partition (int arr[], int low, int high)
     return (low);  
 }  
 
-void quickSort(int *arr, int low, int high)  
+template<typename var>
+void quickSort(var *arr, int low, int high)  
 {  
     if (low < high)  
     {  
